28c9de781c5e5e2bbf46b3b6b01b2f75e995030a
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-10-25 15:12:00 +0300

    commit server

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+const PORT = 3030;
+
+app.get('/', (req, res, next) => {
+	res.send('Hello from RESTful API!');
+})
+
+app.use(routes);
+
+app.listen(PORT, () => { console.log(`Server is listening on port ${PORT} ...`)});

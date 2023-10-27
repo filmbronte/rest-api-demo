75f6e7d212ac225a53088c0295efb5539239a5c6
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-10-27 18:55:54 +0300

    set up cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = 3030;
+
+app.use(cors());
+
+// app.use((req, res, next) => {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader('Access-Control-Allow-Methods', '*');
+// 	res.setHeader('Access-Control-Allow-Headers', '*');
+//
+// 	next();
+// })
 
 app.get('/', (req, res, next) => {
 	res.send('Hello from RESTful API!');

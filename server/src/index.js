55168efbb14d9cbfd9426ad4c261df74606a8aa5
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-10-27 19:00:35 +0300

    add middleware to read body

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,8 @@ const app = express();
 
 const PORT = 3030;
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()); //application/json -> AJAX requests
 app.use(cors());
 
 // app.use((req, res, next) => {
